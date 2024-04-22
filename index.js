0aad8258dc958d63e3509cc89e7e45823d865227
Nischitha sanchi <nischitha1320@gmail.com>
2024-04-22 17:31:27 +0530

    cors commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
-
+import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -18,6 +18,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
